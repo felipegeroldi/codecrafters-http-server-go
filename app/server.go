5a55c39d12b387c6df931bfa7a2a4f287d5fddd7
Felipe Geroldi <142124821+felipegeroldi@users.noreply.github.com>
2024-04-21 16:35:10 -0300

    Handling concurrent connections

--- app/server.go
+++ app/server.go
@@ -20,13 +20,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	HandleConnection(conn)
+		go HandleConnection(conn)
+	}
 }
 
 func HandleConnection(conn net.Conn) {
